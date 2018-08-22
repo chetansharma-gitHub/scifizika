a001815ce20c9b93ee931fce29d3978d70dfd407
Chetan Sharma <sharmachetan187@gmail.com>
2018-08-22 09:16:57 +0530

    Frontend Deployment

---
+++ Scifizika Frontend 18AUG2018/js/main.js
@@ -0,0 +1,21 @@
+/*
+	Author: Yash
+	Created Date: 22/4/18
+	Description: 
+*/
+
+
+//Existing user Login
+function login(){
+	alert('login clicked');
+} 
+
+
+
+//New User Sign Up
+
+function registerMe(){
+	alert('registerMe');
+	console.log('this is yash'+name1);
+
+}
